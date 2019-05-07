e7f0a66fd08cc5472acc557c27f76db0b55c0614
rcrdlbl <bluh@outlook.com>
2019-05-07 18:10:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function depthFirstSearch(rootNode, vertices, edges) {
-  
+  let visited = []
+
+}
+
+function findAdjacent(nodeName,  vertices, edges){
+  return edges.filter(function(edge){
+    return edge.includes(nodeName)
+  }).map(function(edge){
+    return edge.filter(function(node){
+      return (node != nodeName)
+    })[0]
+  }).map(function(name){
+    return findNode(name, vertices)
+  }).filter(function(node){
+		return !node.discovered
+	})
+}
+
+function findNode(nodeName, vertices){
+  return vertices.find(function(vertex){
+    return vertex.name == nodeName
+  })
 }
